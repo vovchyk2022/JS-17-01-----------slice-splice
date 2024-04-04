58d2afb6c42452626382995deda86ffc9f2f5a38
Volodymyr Kapushchak <poshta070887@gmail.com>
2024-04-04 08:32:23 +0300

    Add method pop to Array

--- js/MyArray.js
+++ js/MyArray.js
@@ -1,46 +1,43 @@
-
 // constructor for prototype
+
 function MyArrayPrototype() {
-    this.push = function () {
-    };
-    this.pop = function () {
+  this.push = function () { 
+    //   this[this.length] = value;
+    //       return ++this.length;
+    for (let index = 0; index < arguments.length; index++) {
+      this[this.length] = arguments[index];
+      this.length++;
     }
-this.reverse = function(){
+    return this.length;
+  };
+  this.pop = function () {
+    if (this.length === 0) {
+      return;
     }
-    }
+    const lastItem = this[this.length - 1];
+    delete this[--this.length];
+    // this.length--;
+    return lastItem;
+  };
+}
 
-    // cтворити новий екземпляр нашого масиву
-    const newMyArray = new MyArray();
-    // зпушити в нього елементи з поточного масиву у зворотньому порядку 
-    for (let i = this.length-1; i >=0; i--) {newMyArray.push(this[i])
-    }
-    for (let i = 0, i < this.length; i++){
-        this[i] = newMyArray[i];
-    }
-    // повернути новий екземпляринашого масиву
-    return newMyArray;  
+//constructor with data
+function MyArray() {
+  this.length = 0;
+//   for (let index = 0; index < arguments.length; index++) {
+//     this.push = arguments[index];
+//   }
+}
 
+MyArray.prototype = new MyArrayPrototype();
 
-  //constructor with data
-  function MyArray() {
-      this.length = 0;
-      for (let i = 0; i < arguments.length; i++) {
-        this.push(arguments[i]);
-      }
+const myArrayNumbers = new MyArray(8, 9, 10);
+myArrayNumbers.push(777, 5, 6, 7);
+console.log(myArrayNumbers);
 
-  }
-  
-  MyArray.prototype = new MyArrayPrototype();
-  
+myArrayNumbers.pop();
+console.log(myArrayNumbers);
 
-  const myArrayNumbers = new MyArray(4,5,8,9);
-  myArrayNumbers.push(777);
-const newMyArrayReverse = MyArrayPrototype.reverse();  console.log(myArrayNumbers);
-
-  // myArrayNumbers.pop();
-  // myArrayNumbers.pop();
-  // console.log(myArrayNumbers);
-
-  // const arrayNumbers = [1,2,5,8];
-  // arrayNumbers.push(555);
-  // console.log(arrayNumbers);  
+const arrayNumber = new Array(3, 5, 7);
+arrayNumber.push(555);
+console.log(arrayNumber);
